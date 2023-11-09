4c90060152627844763131ec19811fa5b08296f5
THIRSTY <96413304+showyoursaturn@users.noreply.github.com>
2023-11-09 22:35:53 +0900

    Title: 윤년인가, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231109/윤년인가/is-leap-year.py
@@ -0,0 +1,10 @@
+y = int(input())
+
+if y % 400 == 0 and y % 100 == 0 and y % 4 == 0:
+    print('true')
+elif y % 4 == 0 and y % 100 :
+    print('false')
+elif y % 4 == 0:
+    print('true')
+else:
+    print('flase')
