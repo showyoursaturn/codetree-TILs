41fef843500704bd1773b7c1bcc676dec5f4d9bb
THIRSTY <96413304+showyoursaturn@users.noreply.github.com>
2023-11-09 22:30:32 +0900

    Title: 남녀노소 구분짓기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231109/남녀노소 구분짓기/sex-and-age.py
+++ 231109/남녀노소 구분짓기/sex-and-age.py
@@ -1,6 +1,6 @@
 s, age = map(int,input().split())
 
-if s = 0:
+if s == 0:
     print('MAN') if age >= 19 else print('BOY')
-if s = 1:
+if s == 1:
     print('WOMAN') if age >= 19 else print('GIRL')
