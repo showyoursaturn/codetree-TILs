016088b1f6e49bcf510f0d7f970f16e8dbe20597
THIRSTY <96413304+showyoursaturn@users.noreply.github.com>
2023-11-09 22:24:49 +0900

    Title: 두 사람, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 231109/두 사람/two-person.py
+++ 231109/두 사람/two-person.py
@@ -4,7 +4,7 @@ s1, g1 = input().split()
 s = int(s)
 s1 = int(s1)
 
-if (s >= 19 or s1 >=19) and (g or g1 == 'M'):
+if (s >= 19 and g =='M') or (s1 >= 19 and g1 == 'M'):
     print(1)
 else:
     print(0)
