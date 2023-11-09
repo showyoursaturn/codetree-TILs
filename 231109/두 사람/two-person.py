21c98bce3629f62cfbe5ee7be1835eaa44f3f5e7
THIRSTY <96413304+showyoursaturn@users.noreply.github.com>
2023-11-09 22:11:43 +0900

    Title: 두 사람, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231109/두 사람/two-person.py
+++ 231109/두 사람/two-person.py
@@ -4,7 +4,8 @@ s1, g1 = input().split()
 s = int(s)
 s1 = int(s1)
 
-if (s >= 19 or s1 >= 19) and (g == 'M' or g1 == 'M'):
-    print(1)
-else:
-    print(0)
+if g == 'M' or g1 == 'M':
+    if s >= 19 or s1 <= 19:
+        print(1)
+    else:
+        print(0)
