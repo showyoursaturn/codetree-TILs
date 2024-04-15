81aeb0838e021d1dac26195d31dfb4bd2cc46b0d
THIRSTY <96413304+showyoursaturn@users.noreply.github.com>
2024-04-15 13:05:17 +0900

    Title: a/b 출력, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240415/a/b 출력/a-divide-b.py
+++ 240415/a/b 출력/a-divide-b.py
@@ -1,7 +1,11 @@
 a,b = map(int, input().split())
 m = (a*10)//b
 n = (a*10)%b
-k = "0."
+if a<b :
+    k = "0."
+else:
+    k = str(float(m))
+
 for i in range(20):
     k += str(m)
     m = (n*10)//b
