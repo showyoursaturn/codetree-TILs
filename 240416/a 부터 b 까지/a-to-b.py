e7ae3659f9e2c6d1b5cbdcbfdd04c15ceb27381c
THIRSTY <96413304+showyoursaturn@users.noreply.github.com>
2024-04-16 11:42:42 +0900

    Title: a 부터 b 까지, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240416/a 부터 b 까지/a-to-b.py
+++ 240416/a 부터 b 까지/a-to-b.py
@@ -1,11 +1,11 @@
 a, b = map(int, input().split())
-print(a, end = " ")
 m = a
 for i in range (a, b+1):
-    if m % 2 == 0 and m < b:
-        m = m + 3
+    if m > b:
+        break
+    else:
         print(m, end = " ")
-
-    elif m % 2 == 1 and m < b:
-        m = m*2
-        print(m, end = " ")
+        if m % 2 == 0:
+           m += 3
+        elif m % 2 == 1:
+           m = m * 2
