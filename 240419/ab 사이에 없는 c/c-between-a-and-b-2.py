37ef95a2a2e301431b5832932b83b5f4b50c539b
THIRSTY <96413304+showyoursaturn@users.noreply.github.com>
2024-04-19 09:33:23 +0900

    Title: ab 사이에 없는 c, Time: 141ms, Memory: 31MB, Status: Passed - CodeTree

--- 240419/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240419/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -2,10 +2,10 @@ a,b,c = map(int,input().split())
 call = True
 
 for i in range(a, b+1):
-    if c%i==0:
+    if i%c==0:
         call=False
         break
 if call == False:
-    print("YES")
-else:
     print("NO")
+else:
+    print("YES")
