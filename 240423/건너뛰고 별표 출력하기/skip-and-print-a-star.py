b011e927a056975eeb031213d946738c8a1cafe8
THIRSTY <96413304+showyoursaturn@users.noreply.github.com>
2024-04-23 09:08:59 +0900

    Title: 건너뛰고 별표 출력하기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240423/건너뛰고 별표 출력하기/skip-and-print-a-star.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(1,n+1):
+    for j in range(i):
+        print("*", end = "")
+    print()
+    print()
+for i in range(1,n):
+    for j in range(n-i):
+        print("*", end = "")
+    print()
+    print()
