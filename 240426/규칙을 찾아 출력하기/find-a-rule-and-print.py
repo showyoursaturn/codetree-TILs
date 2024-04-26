58591f9dd531bd2c71e358e267838530aa5ef9f0
THIRSTY <96413304+showyoursaturn@users.noreply.github.com>
2024-04-26 16:03:38 +0900

    Title: 규칙을 찾아 출력하기, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240426/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == n-1:
+            print("*", end = " ")
+        elif i == j or i<j:
+            print(" ", end = " ")
+        else:
+            print("*", end = " ")
+    print()
